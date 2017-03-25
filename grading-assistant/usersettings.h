a2ea2bb0916c362619f6c30ffa678c07135f5ece
Ezekiel Elin <ezekielelin@me.com>
2017-03-25 13:40:52 -0400

    Implemented UserSettings
    This is mostly pulled from lab 2

--- grading-assistant/usersettings.h
+++ grading-assistant/usersettings.h
@@ -1,11 +1,32 @@
 #ifndef USERSETTINGS_H
 #define USERSETTINGS_H
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
 
-class UserSettings
-{
+class UserSettings {
 public:
     UserSettings();
+    UserSettings(std::string path);
+    UserSettings(UserSettings &obj);
+
+    void operator =(UserSettings &obj);
+
+    ~UserSettings();
+
+    void save();
+    void load();
+
+    std::string get_path();
+    void set_path(std::string path);
+
+private:
+    std::map<std::string, std::string> values;
+    std::string path;
+
+    const std::string delimeter = " =: ";
 };
 
 #endif // USERSETTINGS_H
