d497859ad2696042f529c1c876b9b649c48ce1e9
Ezekiel Elin <ezekielelin@me.com>
2017-03-25 14:36:12 -0400

    Cleaned up main.cpp

--- grading-assistant/main.cpp
+++ grading-assistant/main.cpp
@@ -4,35 +4,14 @@
 #include "usersettings.h"
 #include "filemanager.h"
 
-using namespace std;
-
 int main(int argc, char* argv[]) {
 
-    GradingAssistant ga;
-
-    Class* c = new Class();
-    c->set_name("CS205 Section 1");
-
-    Assignment* a = new Assignment();
-    a->set_title("Assignment Title");
-    a->set_description("Cool Description");
-
-    c->add_assignment(a);
-
-    ga.add_class(c);
-
-    std::cout << ga.to_string() << std::endl;
-
-
     FileManager::assure_directory_exists(FileManager::get_data_directory());
-
-    /* User Settings */
+    GradingAssistant ga;
     UserSettings settings(FileManager::get_settings_path());
     settings.load();
 
-    std::cout << settings.get("test") << std::endl;
-    settings.set("test","yay");
-    std::cout << settings.get("test") << std::endl;
+
 
     settings.save();
 
