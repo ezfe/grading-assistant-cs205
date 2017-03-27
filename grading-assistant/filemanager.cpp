6bc2da7686203b70b96574202399a4299ede4a0f
Ezekiel Elin <ezekielelin@me.com>
2017-03-27 15:33:11 -0400

    Cleaned up FileManager

--- grading-assistant/filemanager.cpp
+++ grading-assistant/filemanager.cpp
@@ -5,16 +5,19 @@ FileManager::FileManager() {
 }
 
 std::string FileManager::expand_home(std::string path) {
-    if (path.at(0) == '~' && path.at(1) == '/') {
+    QString qpath = QString::fromStdString(path);
+    QString ret_val;
+    if (qpath.at(0) == '~' && qpath.at(1) == '/') {
         char* temp = getenv("HOME");
         if (temp != NULL) {
-            std::string home_path = std::string(temp);
-            return home_path + path.substr(1);
+            QString userPath = QString(temp);
+            return QDir::cleanPath(userPath + "/" + qpath.right(qpath.length() - 1)).toStdString();
         } else {
             std::cerr << "Unable to get HOME" << std::endl;
+            return QDir::cleanPath(qpath).toStdString();
         }
     } else {
-        return path;
+        return QDir::cleanPath(qpath).toStdString();
     }
 }
 
@@ -29,7 +32,7 @@ std::string FileManager::get_app_directory() {
     const std::string app_name = "elin-sampsell.grading-assistant";
     std::string ret_val;
     if (GA_PLATFORM == GA_PLATFORM_APPLE) {
-        ret_val = FileManager::expand_home("~/Library/Application Support//" + app_name + "/");
+        ret_val = FileManager::expand_home("~/Library/Application Support/" + app_name + "/");
     } else if (GA_PLATFORM == GA_PLATFORM_LINUX) {
         ret_val = FileManager::expand_home("~/." + app_name + "/");
     } else {
@@ -39,9 +42,9 @@ std::string FileManager::get_app_directory() {
 }
 
 std::string FileManager::get_settings_path() {
-    return QDir::cleanPath(QString::fromStdString(FileManager::get_app_directory()) + QDir::separator() + "settings.txt").toStdString();
+    return QDir::cleanPath(QString::fromStdString(FileManager::get_app_directory() + "/settings.txt")).toStdString();
 }
 
 std::string FileManager::get_database_path() {
-    return QDir::cleanPath(QString::fromStdString(FileManager::get_app_directory()) + QDir::separator() + "database.sqlite3").toStdString();
+    return QDir::cleanPath(QString::fromStdString(FileManager::get_app_directory() + "/database.sqlite3")).toStdString();
 }
