0e5e0399d6a531abfc4facd7bf6faeb1ec8f43c4
Unknown <sampseln@lafayette.edu>
2017-03-27 23:43:15 -0400

    Implemented Rubric classes
    -rubric has vector of rubric rows
    -current design assumes user will enter all rubric information at once (descriptions + category values known)

---
+++ grading-assistant/garubric.cpp
@@ -0,0 +1,43 @@
+#include "garubric.h"
+
+GARubric::GARubric(){
+
+}
+
+GARubric::~GARubric()
+{
+    for(GARubricRow* row: this->rows) {
+        delete row;
+    }
+    delete ec;
+}
+
+std::string GARubric::get_title() {
+    return title;
+}
+
+void GARubric::set_title(std::string t) {
+    title = t;
+}
+
+std::vector<GARubricRow *> GARubric::get_rows() {
+    return rows;
+}
+
+void GARubric::add_row(std::string category, std::vector<std::string> descriptions,
+                       int pointValue) {
+    rows.push_back(new GARubricRow(category, descriptions, pointValue));
+}
+
+void GARubric::add_ec(std::string c, std::string description, int pointValue) {
+
+    ec = new GARubricRow(c, description, pointValue);
+}
+
+double GARubric::calculate_score() {
+    double total = 0;
+    for(GARubricRow* row : this->rows) {
+        total += row->get_earned_points();
+    }
+    return (total + ec->get_earned_points())/maxPoints;
+}
---
+++ grading-assistant/garubricrow.cpp
@@ -0,0 +1,39 @@
+#include "garubricrow.h"
+
+GARubricRow::GARubricRow() {
+
+}
+
+GARubricRow::GARubricRow(std::string c, std::string d, int p) {
+    category = c;
+    descriptions.push_back(d);
+    earnedPoints = p;
+}
+
+GARubricRow::GARubricRow(std::string c, std::vector<std::string> d, int p) {
+    category = c;
+    descriptions = d;
+    maxPoints = p;
+    earnedPoints = 0;
+}
+
+std::string GARubricRow::get_category() {
+    return category;
+}
+
+std::vector<std::string> GARubricRow::get_descriptions() {
+    return descriptions;
+}
+
+int GARubricRow::get_max_points() {
+    return maxPoints;
+}
+
+int GARubricRow::get_earned_points() {
+    return earnedPoints;
+}
+
+void GARubricRow::set_earned_points(int p) {
+    earnedPoints = p;
+}
+
