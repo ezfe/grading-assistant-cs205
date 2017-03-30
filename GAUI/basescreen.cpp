c2619277754f72f15db73647a8e8dd27fcc380db
Unknown <sampseln@lafayette.edu>
2017-03-30 18:50:21 -0400

    Switching between key screens implemented

--- GAUI/basescreen.cpp
+++ GAUI/basescreen.cpp
@@ -6,6 +6,7 @@ BaseScreen::BaseScreen(QWidget *parent) :
     ui(new Ui::BaseScreen)
 {
     ui->setupUi(this);
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
 BaseScreen::~BaseScreen()
@@ -15,25 +16,49 @@ BaseScreen::~BaseScreen()
 
 void BaseScreen::on_actionBack_triggered()
 {
-
+    if(ui->stackedWidget->currentIndex() == 0)
+    {
+        return;
+    }
+    else if(ui->stackedWidget->currentIndex() == 1)
+    {
+        ui->stackedWidget->setCurrentIndex(0);
+    }
+    else if(ui->stackedWidget->currentIndex() == 2)
+    {
+        ui->stackedWidget->setCurrentIndex(1);
+    }
+    else if(ui->stackedWidget->currentIndex() == 3)
+    {
+        ui->stackedWidget->setCurrentIndex(2);
+    }
+    else if(ui->stackedWidget->currentIndex() == 4)
+    {
+        ui->stackedWidget->setCurrentIndex(0);
+    }
+    else
+    {
+        return;
+    }
 }
 
 void BaseScreen::on_actionClasses_triggered()
 {
-
+    ui->stackedWidget->setCurrentIndex(1);
 }
 
 void BaseScreen::on_actionRubrics_triggered()
 {
-
+    ui->stackedWidget->setCurrentIndex(4);
 }
 
 void BaseScreen::on_actionCurrent_Session_triggered()
 {
-
+    //current session window
 }
 
 void BaseScreen::on_actionQuit_triggered()
 {
-
+    //save and quit
+    close();
 }
