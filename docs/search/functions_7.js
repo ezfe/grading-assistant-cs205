5fce0dd5d50cb6a9fd57225e122bb12fe242adbc
Ezekiel Elin <ezekielelin@me.com>
2017-04-10 15:56:07 -0400

    Comments on GARubric

--- docs/search/functions_7.js
+++ docs/search/functions_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['load',['load',['../class_grading_assistant.html#a2430309557f1fd413a75165b4c7c9a3a',1,'GradingAssistant']]],
-  ['load_5ffrom',['load_from',['../class_g_a_annotation.html#acbf5c9a4f95f75d7a7cf7cc6f43e00b5',1,'GAAnnotation::load_from()'],['../class_g_a_assignment.html#a167d821c1311025490e4bbbbac6d8ad4',1,'GAAssignment::load_from()'],['../class_g_a_assignment_data.html#a31ed802f1ef4b21bd9f47024a80bf4bd',1,'GAAssignmentData::load_from()'],['../class_g_a_student.html#a7b95740499af16d32f2b12ec2508ee7e',1,'GAStudent::load_from()']]]
+  ['load_5ffrom',['load_from',['../class_g_a_annotation.html#acbf5c9a4f95f75d7a7cf7cc6f43e00b5',1,'GAAnnotation::load_from()'],['../class_g_a_assignment.html#a167d821c1311025490e4bbbbac6d8ad4',1,'GAAssignment::load_from()'],['../class_g_a_assignment_data.html#a31ed802f1ef4b21bd9f47024a80bf4bd',1,'GAAssignmentData::load_from()'],['../class_g_a_rubric.html#ac7180d84cb7943e26f90bc43b9c19d03',1,'GARubric::load_from()'],['../class_g_a_student.html#a7b95740499af16d32f2b12ec2508ee7e',1,'GAStudent::load_from()']]]
 ];
