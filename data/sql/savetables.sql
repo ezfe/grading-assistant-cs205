a80e2d61f934744c7e69973b487776d8845752a1
Adam Ness <nessa@lafayette.edu>
2017-04-12 21:31:22 -0400

    Fixed issue with .csv export from database.

--- data/sql/savetables.sql
+++ data/sql/savetables.sql
@@ -1,35 +1,37 @@
 .mode csv
 .separator ","
-/*
+
 .output ../grading-assistant-cs205/data/tables/annotations.csv
 
 SELECT * FROM Annotations;
-*/
-.output ../grading-assistant-cs205/data/mocktables/assignments.csv 
+
+.output ../grading-assistant-cs205/data/tables/assignments.csv 
 
 SELECT * FROM Assignments;
 
-.output ../grading-assistant-cs205/data/mocktables/assignmentdata.csv
+.output ../grading-assistant-cs205/data/tables/assignmentdata.csv
 
 SELECT * FROM AssignmentData;
 
-.output ../grading-assistant-cs205/data/mocktables/classes.csv
+.output ../grading-assistant-cs205/data/tables/classes.csv
 
 SELECT * FROM Classes;
 
-.output ../grading-assistant-cs205/data/mocktables/rubrics.csv
+.output ../grading-assistant-cs205/data/tables/rubrics.csv
 
 SELECT * FROM Rubrics;
 
-.output ../grading-assistant-cs205/data/mocktables/rubricrows.csv
+.output ../grading-assistant-cs205/data/tables/rubricrows.csv
 
 SELECT * FROM RubricRows;
 
-.output ../grading-assistant-cs205/data/mocktables/rubricrowvalues.csv
+.output ../grading-assistant-cs205/data/tables/rubricrowvalues.csv
 
 SELECT * FROM RubricRowValues;
 
-.output ../grading-assistant-cs205/data/mocktables/students.csv
+.output ../grading-assistant-cs205/data/tables/students.csv
+
+SELECT * FROM Students;
 
 .exit
 
