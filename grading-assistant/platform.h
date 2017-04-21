af2360421e2b191a94cb446643fe952b5ea0855c
Ezekiel Elin <ezekielelin@me.com>
2017-04-21 14:59:52 -0400

    Changes to platform

--- grading-assistant/platform.h
+++ grading-assistant/platform.h
@@ -8,16 +8,16 @@
 
 #ifdef __APPLE__
     #define GA_PLATFORM GA_PLATFORM_APPLE
-    #define GA_APPLE_ACTIVE
+    #define GA_APPLE_ACTIVE true
 #elif _WIN32
     #define GA_PLATFORM GA_PLATFORM_WINDOWS
-    #define GA_WINDOWS_ACTIVE
+    #define GA_WINDOWS_ACTIVE true
 #elif __linux__
     #define GA_PLATFORM GA_PLATFORM_LINUX
-    #define GA_LINUX_ACTIVE
+    #define GA_LINUX_ACTIVE true
 #else
     #define GA_PLATFORM GA_PLATFORM_UNKNOWN
-    #define GA_UNKNOWN_ACTIVE
+    #define GA_UNKNOWN_ACTIVE true
 #endif
 
 #endif // PLATFORM_H
